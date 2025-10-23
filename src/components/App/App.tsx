463014e31452e2f39a2daef994e48f85c8d48395
bartoszb0 <bartoszb2020@gmail.com>
2025-10-23 19:56:10 +0200

    added reseting localStorage everyday

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -8,7 +8,10 @@ import "./App.css";
 
 function App() {
   const [rappers] = useState(rappersArray);
-  const [guessedRappers, setGuessedRappers] = useState<Rapper[]>([]);
+  const [guessedRappers, setGuessedRappers] = useState<Rapper[]>(() => {
+    const guessedRappers = localStorage.getItem("guessedRappers");
+    return guessedRappers ? JSON.parse(guessedRappers) : [];
+  });
   const [guessing, setGuessing] = useState(false);
   const todaysRapper = rappers[1]; // for now
   const [gameWon, setGameWon] = useState<boolean>(() => {
@@ -19,10 +22,23 @@ function App() {
   // probably should add here input ref to pass it
   // down to Guesses so its possible to focus on it after guessing
 
-  // localStorage implementation
+  // Reset localStorage everyday
+  useEffect(() => {
+    const today = new Date().toDateString();
+    const lastReset = localStorage.getItem("lastResetDate");
 
+    if (lastReset !== today) {
+      localStorage.clear();
+      localStorage.setItem("lastResetDate", today);
+      setGameWon(false);
+      setGuessedRappers([]);
+    }
+  }, []);
+
+  // LocalStorage implementation
   useEffect(() => {
     localStorage.setItem("gameWon", JSON.stringify(gameWon));
+    localStorage.setItem("guessedRappers", JSON.stringify(guessedRappers));
   }, [guessedRappers, gameWon]);
 
   return (
